94f586c0cc7c37fe3a0b90bd6a88d3cd2bc1f762
Joffrey F <jfuhrer@brandless.com>
2019-05-26 14:57:12 -0700

    Bump version

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
         Spell finder{' '}
         <small><a className="text-white" href="http://paizo.com/pathfinderplaytest">Pathfinder Playtest</a></small>
       </h1>
-      <div><a className="text-white" href="/Changelog.txt">v1.5.1 (Changelog)</a></div>
+      <div><a className="text-white" href="/Changelog.txt">v2.0.0 (Changelog)</a></div>
     </header>
   );
 }
